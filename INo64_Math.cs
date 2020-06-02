8aa11854094b54776ee4679b801cafa8a41811e5
atwood <atwood.so.interesting@gmail.com>
2020-06-02 17:26:54 +0800

    solved two more middle, only two hard math question last were left

---
+++ INo64_Math.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_I64
+{
+    //static void Main(string[] args)
+    //{
+    //    var solution = new Solution();
+    //    while (true)
+    //    {
+    //        int input = int.Parse(Console.ReadLine());
+    //        //int input2 = int.Parse(Console.ReadLine());
+    //        //string input = Console.ReadLine();
+    //        //string input2 = Console.ReadLine();
+    //        //int[] intArr = input.Split(',').Select(s => int.Parse(s)).ToArray();
+    //        //int input = int.Parse(input2);
+    //        var res = solution.SumNums(input);
+    //        Console.WriteLine(res);
+    //    }
+    //}
+
+    /// <summary>
+    /// Unsolved Question: 为什么使用了类中全局的变量，避免了递归每次申请变量，反而内存消耗变大了。这个递归应该会申请很多个bool才对啊
+    /// </summary>
+    public class Solution
+    {
+        /// 这个变量放在外面内存消耗会大一些,大了0.1-0.2MB,没有搞懂
+        private bool _uselessTemp;
+        /// <summary>
+        /// 递归获取
+        /// 时间复杂度：O(n)。递归函数递归 nn 次，每次递归中计算时间复杂度为 O(1)，因此总时间复杂度为 O(n)。
+        /// 空间复杂度：O(n)。递归函数的空间复杂度取决于递归调用栈的深度，这里递归函数调用栈深度为 O(n)，因此空间复杂度为 O(n)。
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int SumNums(int n)
+        {
+            _uselessTemp = (n > 0) && (n += SumNums(n - 1)) > 0;
+            return n;
+        }
+
+        /// <summary>
+        /// 俄罗斯农民乘法（和我第一反应很相似），主要使用等差数列求和公式1+2+⋯+n 等价于(n(n+1))/2。
+        /// 时间复杂度：O(logn)。快速乘需要的时间复杂度为 O(logn)。因为是位移操作，是指数级下降
+        /// 空间复杂度：O(1)。只需要常数空间存放若干变量。
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        //public int SumNums(int n)
+        //{
+        //    int m = n + 1;
+        //    int res = 0;
+        //    bool uselessTemp;
+        //    //1 <= n <= 10000,第十五位代表的是16384,那么十四位就可以代表（16383）包含10000,所以左移14次就可以获得答案
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    uselessTemp = (m & 1) == 1 && (res += n) > 0;
+        //    n <<= 1;
+        //    m >>= 1;
+
+        //    return res >> 1;
+        //}
+    }
+}
---
+++ No43_Math.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_43
+{
+    //static void Main(string[] args)
+    //{
+    //    var solution = new Solution();
+    //    while (true)
+    //    {
+    //        //int input = int.Parse(Console.ReadLine());
+    //        //int input2 = int.Parse(Console.ReadLine());
+    //        string input = Console.ReadLine();
+    //        string input2 = Console.ReadLine();
+    //        //int[] intArr = input.Split(',').Select(s => int.Parse(s)).ToArray();
+    //        //int input = int.Parse(input2);
+    //        var res = solution.Multiply(input, input2);
+    //        Console.WriteLine(res);
+    //    }
+    //}
+
+    /// <summary>
+    /// Experience: 
+    /// 1.用数组来进行加减时，改变向左进位的思维，向右进位，因为数组利用【0】的位置来对齐是更方便的做法，否则要把短的数据用零补齐。
+    /// 2.数组加减之后小心进位，整个数组循环完之后应该判断一次进位符是否为零，不为零就加在数组后面。
+    /// </summary>
+
+    public class Solution
+    {
+        private List<int> _res = new List<int>();
+
+        /// <summary>
+        /// 使用小学竖式乘法来计算，算法其实不难，主要是实现中遇到的各种细节。这种就应该先写好，详细到遍历顺序，否则后来会疯狂debug
+        /// 时间复杂度：O(mn),因为两个string输入多长，就要计算多少次
+        /// 空间复杂度：O(Max(m,n)),只用了一个数组来存结果。但是我看之前计算空间复杂度的有说，如果给定了长度，可以考虑成只使用了常量个。。。如果这样算的话，就是O(1)，嗯，仔细想想并不是这样的吧，哈哈哈，这尼玛也太厚脸皮了吧，还O(1)
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <returns></returns>
+        public string Multiply(string num1, string num2)
+        {
+            if (num1 == "0" || num2 == "0")
+                return "0";
+
+            for (int i = num2.Length - 1; i >= 0; i--)
+            {
+                MultipleForStr(num1, num2[i], num2.Length - 1 - i);
+            }
+            //把整型数组转化为string
+            StringBuilder sb = new StringBuilder();
+            for (int i = _res.Count - 1; i >= 0; i--)
+            {
+                sb.Append(_res[i]);
+            }
+            return sb.ToString();
+        }
+
+        private void MultipleForStr(string num1, char n, int pow)
+        {
+            List<int> stepRes = new List<int>();
+            int carry = 0;
+            for (int i = num1.Length - 1; i >= 0; i--)
+            {
+                int cur_res = (num1[i] - '0') * (n - '0') + carry;
+                stepRes.Add((cur_res) % 10);
+                carry = cur_res / 10;
+            }
+            //如果还有要进位的
+            if (carry != 0)
+            {
+                stepRes.Add(carry);
+            }
+            stepRes.InsertRange(0, new int[pow]);
+            PlusAndAssignToGlobal(stepRes);
+        }
+
+        private void PlusAndAssignToGlobal(List<int> stepRes)
+        {
+            int carry = 0;
+            int index = Math.Max(_res.Count, stepRes.Count);
+            for (int i = 0; i < index; i++)
+            {
+                //如果全局长度不够，就给他补齐一个初始化为0的数组
+                if (_res.Count < i + 1)
+                {
+                    _res.AddRange(new int[i + 1 - _res.Count]);
+                }
+                int plus_res = _res[i] + stepRes[i] + carry;
+                _res[i] = plus_res % 10;
+                carry = plus_res / 10;
+            }
+            //如果还有要进位的
+            if (carry != 0)
+            {
+                _res.Add(carry);
+            }
+        }
+    }
+}
