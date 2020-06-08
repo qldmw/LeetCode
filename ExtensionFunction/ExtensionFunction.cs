2f03858f05dad3f8bc8131ec3c6e8ad710d5a2c7
atwood <atwood.so.interesting@gmail.com>
2020-06-08 19:50:33 +0800

    upgrade ConsoleX.WriteLine

--- ExtensionFunction/ExtensionFunction.cs
+++ ExtensionFunction/ExtensionFunction.cs
@@ -1,41 +1,87 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace System.Extension
-{    
+{
+    /// <summary>
+    /// Experience：
+    /// 1.[]是IList的方法。
+    /// 2.Count是ICollection的方法。
+    /// 3.遍历（可不可以foreach）是IEnumerable的方法。
+    /// 4.List 和 Array（就是int[]这种）都实现了IList, IEnumerable, ICollection，继承关系大概总结为 IList > ICollection > IEnumerable。
+    /// 5.List<T>可以转为IList<T>，是因为List实现了IList;List<List<T>>不能转化为IList<IList<T>>，是因为List<T>的泛型参数不支持协变(Covariance)和逆变(Contravariance)，
+    ///   支持协变的IEnumerable接口就可以完成这个转换，IEnumerable<List<T>>就可以转换为IEnumerable<IList<T>>。
+    /// 6.对于协变和逆变的个人总结：
+    ///   协变:子类可以变为父类的这个过程，比如List实现了IList，IList实现了IEnumerable，这种可以变过去。但是IList不能变成List，因为实现IList的
+    ///        不只有一种类型，可能是List，但也可能是Array，还可能是其他的，这样编译器就不能确定，所以不能转换。
+    ///   逆变:父类变为某个子类，但是因为编译器是不知道你要做什么的，所以要你去实现这个过程，到底要怎么做。但是ICompare之类的接口是天然支持逆变的，可能因为对于比较来说，类型
+    ///        变化始终如一吧？
+    /// </summary>
     public static class ConsoleX
     {
-        public static void WriteLine(bool[] arr)
+        /// <summary>
+        /// 打印数组（支持一维和二维）
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arr"></param>
+        public static void WriteLine<T>(T arr) where T : IEnumerable
         {
-            string str = "[";
-            for (int i = 0; i < arr.Length; i++)
+            //通过第一个儿子确定是一维还是二维的
+            if (IsChildEnumerable(arr))
             {
-                str += $"{arr[i]},";
+                foreach (var m in arr)
+                {
+                    IEnumerable IEnum = m as IEnumerable;
+                    WriteLineBase(IEnum);
+                }
             }
-            str = str.TrimEnd(',') + "]";
-            Console.WriteLine(str);
+            else
+            {
+                WriteLineBase(arr);
+            }
         }
 
-        public static void WriteLine(int[] arr)
+        /// <summary>
+        /// 是否是二维数组（指两层 交错数组 和 嵌套IEnumerable），通过遍历儿子是否是IEnumerable来确定
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arr"></param>
+        /// <returns></returns>
+        private static bool IsChildEnumerable<T>(T arr) where T : IEnumerable
         {
-            string str = "[";
-            for (int i = 0; i < arr.Length; i++)
+            if (arr == null)
+                return false;
+
+            bool isEnum = false;
+            foreach (var m in arr)
             {
-                str += $"{arr[i]},";
+                if (m is IEnumerable)
+                {
+                    isEnum = true;
+                    break;
+                }
             }
-            str = str.TrimEnd(',') + "]";
-            Console.WriteLine(str);
+            return isEnum;
         }
 
-        public static void WriteLine(string[] arr)
+        /// <summary>
+        /// 打印数组的具体方法
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="arr"></param>
+        private static void WriteLineBase<T>(T arr) where T : IEnumerable
         {
             string str = "[";
-            for (int i = 0; i < arr.Length; i++)
+            //类型实现了IEnumerate不等于就有值
+            if (arr != null)
             {
-                str += $"{arr[i]},";
+                foreach (var m in arr)
+                {
+                    str += $"{m},";
+                }
             }
             str = str.TrimEnd(',') + "]";
             Console.WriteLine(str);
--- Program.cs
+++ Program.cs
@@ -14,106 +14,24 @@ namespace LeetCode
             var solution = new Solution();
             while (true)
             {
-                //int input = int.Parse(Console.ReadLine());
+                int input = int.Parse(Console.ReadLine());
                 //int input2 = int.Parse(Console.ReadLine());
                 //int input3 = int.Parse(Console.ReadLine());
                 //string input = Console.ReadLine();
                 //string input2 = Console.ReadLine();
                 //int[] intArr = input.Split(',').Select(s => int.Parse(s)).ToArray();
-                //int input = int.Parse(input2);
-                //int[][] intArr = new int[][]
-                //{
-                //    new int[]{1,2,3,4},
-                //    new int[]{5,6,7,8},
-                //    new int[]{9,10,11,12},
-                //};
-                //var intArr1 = new int[] { 1, 2, 3, 0, 0, 0 };
-                //var intArr2 = new int[] { 2, 5, 6 };
-                //int m = 3;
-                //int n = 3;
-                //var intArr1 = new int[1];
-                //var intArr2 = new int[] { 1 };
-                //int m = 0;
-                //int n = 1;
-                var intArr1 = new int[] { 2, 0 };
-                var intArr2 = new int[] { 1 };
-                int m = 1;
-                int n = 1;
-                solution.Merge(intArr1, m, intArr2, n);
-                //ConsoleX.WriteLine(res);
+                //int input = int.Parse(input2);                
+                var res = solution.Generate(input);
+                ConsoleX.WriteLine(res);
             }
         }
 
         public class Solution
         {
-            /// 优化做法，从后往前填，就直接在m数组上写，这样就不用tolist，而且少了一次之前的把数据从list的地址转到nums1的地址。
-            /// 时间复杂度：O(m + n)
-            /// 空间复杂度：O(1)
-            public void Merge(int[] nums1, int m, int[] nums2, int n)
+            public IList<IList<int>> Generate(int numRows)
             {
-                int p1 = m - 1;
-                int p2 = n - 1;
-                int index = m + n - 1;
-                while (p2 >= 0)
-                {
-                    if (p1 >= 0 && (nums1[p1] > nums2[p2]))
-                        nums1[index--] = nums1[p1--];
-                    else
-                        nums1[index--] = nums2[p2--];
-                }                
+                return null;
             }
-
-            /// <summary>
-            /// 第一反应解，就是单纯的插入，唯一有点优点的就是记住了插入的下标，不用每次都从头插入
-            /// 时间复杂度：O(m + n)，但是我的写法为了让leetcode检测到又多了一次 m 个的循环，可以优化的
-            /// 空间复杂度：O(m + n),图方便用了list，不用的话空间复杂度能到O(1)            
-            /// </summary>
-            /// <param name="nums1"></param>
-            /// <param name="m"></param>
-            /// <param name="nums2"></param>
-            /// <param name="n"></param>
-            //public void Merge(int[] nums1, int m, int[] nums2, int n)
-            //{
-            //    var list1 = nums1.Take(m).ToList();
-            //    //nums1中的插入指针
-            //    int index = 0;
-            //    for (int i = 0; i < n; i++)
-            //    {
-            //        for (int j = index; j < list1.Count; j++)
-            //        {
-            //            //小于等于时在原位置上插入
-            //            if (nums2[i] <= list1[j])
-            //            {
-            //                list1.Insert(j, nums2[i]);
-            //                index = j;
-            //                break;
-            //            }
-            //            else if ((nums2[i] > list1[j] && (j < list1.Count - 1 && nums2[i] <= list1[j + 1]))
-            //                || (j == list1.Count - 1))
-            //            {
-            //                list1.Insert(j + 1, nums2[i]);
-            //                index = j + 1;
-            //                break;
-            //            }
-            //        }
-            //    }
-            //    if (m == 0)
-            //    {
-            //        for (int i = 0; i < n; i++)
-            //        {
-            //            nums1[i] = nums2[i];
-            //        }
-            //    }
-            //    else
-            //    {
-            //        for (int i = 0; i < m + n; i++)
-            //        {
-            //            nums1[i] = list1[i];
-            //        }
-            //    }
-            //    //为什么不能这么写，因为leetcode检查的是nums1的原内存地址
-            //    //nums1 = list1.ToArray();
-            //}
         }
     }
 }
