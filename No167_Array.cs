ad6717097340a17d8f2df40f570bd46d469858d9
qldmw <atwood.l.lu@qq.com>
2020-06-10 01:17:15 +0800

    owe 6 questions

---
+++ No167_Array.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_167
+{
+    //static void Main(string[] args)
+    //{
+    //    var solution = new Solution();
+    //    while (true)
+    //    {
+    //        //int input = int.Parse(Console.ReadLine());
+    //        //int input2 = int.Parse(Console.ReadLine());
+    //        //int input3 = int.Parse(Console.ReadLine());
+    //        string input = Console.ReadLine();
+    //        //string input2 = Console.ReadLine();
+    //        int[] intArr = input.Split(',').Select(s => int.Parse(s)).ToArray();
+    //        int input2 = int.Parse(Console.ReadLine());
+    //        var res = solution.TwoSum(intArr, input2);
+    //        ConsoleX.WriteLine(res);
+    //        //ConsoleX.WriteLine(res);
+    //    }
+    //}
+
+    /// <summary>
+    /// Experience：但凡见到有序数组的查找，就要直接想到二分法。(ps.其实也不一定啦，双指针夹逼也是可以的，不过最基本要想到二分，因为怎么也比遍历快)
+    /// </summary>
+    public class Solution
+    {
+        /// <summary>
+        /// 双指针夹逼
+        /// 时间复杂度：O(n)
+        /// 空间复杂度：O(1)
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+            while (left < right)
+            {
+                int sum = numbers[left] + numbers[right];
+                if (sum == target)
+                    break;
+                else if (sum > target)
+                    right--;
+                else
+                    left++;
+            }
+            return new int[2] { left + 1, right + 1 };
+        }
+
+        /// <summary>
+        /// 第一反应解，遍历 + 二分法
+        /// 时间复杂度：O(nlogn),遍历是n,二分法是logn
+        /// 空间复杂度：O(1)
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        //public int[] TwoSum(int[] numbers, int target)
+        //{
+        //    int[] res = new int[2];
+        //    for (int i = 0; i < numbers.Length; i++)
+        //    {
+        //        int targerIndex = Recursive(numbers, i + 1, numbers.Length - 1, target - numbers[i]);
+        //        if (targerIndex != -1)
+        //        {
+        //            res[0] = i + 1;
+        //            res[1] = targerIndex + 1;
+        //            break;
+        //        }
+        //    }
+        //    return res;
+        //}
+
+        //private int Recursive(int[] numbers, int left, int right, int target)
+        //{
+        //    int mid = (left + right) / 2;
+        //    if (numbers[mid] == target)
+        //        return mid;
+        //    else if (left >= right)
+        //        return -1;
+
+        //    if (numbers[mid] > target)
+        //        return Recursive(numbers, left, mid - 1, target);
+        //    else
+        //        return Recursive(numbers, mid + 1, right, target);
+        //}
+    }
+}
--- Program.cs
+++ Program.cs
@@ -20,53 +20,89 @@ namespace LeetCode
                 string input = Console.ReadLine();
                 //string input2 = Console.ReadLine();
                 int[] intArr = input.Split(',').Select(s => int.Parse(s)).ToArray();
-                int input2 = int.Parse(Console.ReadLine());
-                var res = solution.TwoSum(intArr, input2);
-                ConsoleX.WriteLine(res);
-                //ConsoleX.WriteLine(res);
+                //int input2 = int.Parse(Console.ReadLine());
+                var res = solution.MajorityElement(intArr);
+                Console.WriteLine(res);
             }
         }
 
-        /// <summary>
-        /// Experience：但凡见到有序数组的查找，就要直接想到二分法
-        /// </summary>
         public class Solution
         {
             /// <summary>
-            /// 第一反应解，遍历 + 二分法
+            /// boyer-moore投票算法，通过一个巧妙的方式获取到过半的数
+            /// 时间复杂度:O(n),毋庸置疑的n,看起来应该比之前的遍历统计快不少才对，然而C#执行下来却和之前的遍历统计差不多，运气不好的时候还是一样的，感觉代码优化对C#不太明显，不知道为啥
+            /// 空间复杂度:O(1),常数个变量，然后执行结果和遍历统计完全差不多，都分别执行三次，就有一次内存少了0.1m,其他两次都是一样的
+            /// Unsolved Question：为什么C#执行效率波动那么大，有时候会波动好几十ms；而且空间复杂度明明从O(n)减少到了O(1),但是效果却不明显，是因为用例的问题么，这也差的太少了吧，还不如一个执行波动来的大
             /// </summary>
-            /// <param name="numbers"></param>
-            /// <param name="target"></param>
+            /// <param name="nums"></param>
             /// <returns></returns>
-            public int[] TwoSum(int[] numbers, int target)
+            public int MajorityElement(int[] nums)
             {
-                int[] res = new int[2];
-                for (int i = 0; i < numbers.Length; i++)
+                int candidate = 0;
+                int count = 0;
+                for (int i = 0; i < nums.Length; i++)
                 {
-                    int targerIndex = Recursive(numbers, i + 1, numbers.Length - 1, target - numbers[i]);
-                    if (targerIndex != -1)
-                    {
-                        res[0] = i + 1;
-                        res[1] = targerIndex + 1;
-                        break;
-                    }
+                    if (count == 0)
+                        candidate = nums[i];
+                    
+                    if (nums[i] != candidate)
+                        count--;
+                    else
+                        count++;                    
                 }
-                return res;
+                return candidate;
             }
 
-            private int Recursive(int[] numbers, int left, int right, int target)
-            {
-                int mid = (left + right) / 2;
-                if (numbers[mid] == target)
-                    return mid;
-                else if (left >= right)
-                    return -1;
-                
-                if (numbers[mid] > target)
-                    return Recursive(numbers, left, mid - 1, target);
-                else
-                    return Recursive(numbers, mid + 1, right, target);
-            }
+            /// <summary>
+            /// 第一反应解优化，在加数组中去检查真没必要，虽然最优情况下可能会很好，但是不值得，最后检查一次就可以了
+            /// 时间复杂度：O(n)，遍历n, 插入dictionary常数时间，最后遍历dictionary也是O(n),所以最后是O(n)
+            /// 空间复杂度：O(n)，虽然肯定是小于n的，但是一定是跟n成线性关系的，所以是O(n)
+            /// </summary>
+            /// <param name="nums"></param>
+            /// <returns></returns>
+            //public int MajorityElement(int[] nums)
+            //{
+            //    var dic = new Dictionary<int, int>();
+            //    for (int i = 0; i < nums.Length; i++)
+            //    {
+            //        if (dic.ContainsKey(nums[i]))
+            //            dic[nums[i]]++;
+            //        else
+            //            dic.Add(nums[i], 1);
+            //    }
+
+            //    var marjority = dic.FirstOrDefault(s => s.Value > nums.Length / 2);
+            //    if (marjority.Value != 0)
+            //        return marjority.Key;
+            //    return 0;
+            //}
+
+            /// <summary>
+            /// 第一反应解，计数，过半的时候再开始检查是否有合适的数
+            /// 时间复杂度：O(n²),dictionary中的FirstOrDefault应该是会遍历的，所以最大项应该是n²
+            /// 空间复杂度：O(n)，虽然肯定是小于n的，但是一定是跟n成线性关系的，所以是O(n)
+            /// </summary>
+            /// <param name="nums"></param>
+            /// <returns></returns>
+            //public int MajorityElement(int[] nums)
+            //{
+            //    var dic = new Dictionary<int, int>();
+            //    for (int i = 0; i < nums.Length; i++)
+            //    {
+            //        if (dic.ContainsKey(nums[i]))
+            //            dic[nums[i]]++;
+            //        else
+            //            dic.Add(nums[i], 1);
+
+            //        if (i >= nums.Length / 2)
+            //        {
+            //            var marjority = dic.FirstOrDefault(s => s.Value > nums.Length / 2);
+            //            if (marjority.Value != 0)
+            //                return marjority.Key;
+            //        }
+            //    }
+            //    return 0;
+            //}
         }
     }
 }
