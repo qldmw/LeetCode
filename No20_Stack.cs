5d332639526659df7162c6ab86d2af6a53cdb428
atwood <atwood.so.interesting@gmail.com>
2020-06-16 21:46:13 +0800

    owe 11, Fxxk

---
+++ No20_Stack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_20
+{
+    //static void Main(string[] args)
+    //{
+    //    var solution = new Solution();
+    //    while (true)
+    //    {
+    //        //int input = int.Parse(Console.ReadLine());
+    //        //int input2 = int.Parse(Console.ReadLine());
+    //        //int input3 = int.Parse(Console.ReadLine());
+    //        string input = Console.ReadLine();
+    //        ////string input2 = Console.ReadLine();
+    //        //int[] intArr = input.Split(',').Select(s => int.Parse(s)).ToArray();
+    //        //int input2 = int.Parse(Console.ReadLine());
+    //        //int[] intArr = new int[] { 1, 3, 2 };
+    //        //int[] intArr = new int[] { 4, 2, 1, 3, 2, 6, 3 };
+    //        //int[] intArr2 = new int[] { 4, 2, 1, 3, 2, 6, 3 };
+    //        var res = solution.IsValid(input);
+    //        ConsoleX.WriteLine(res);
+    //    }
+    //}
+
+    public class Solution
+    {
+        /// <summary>
+        /// 犹记得以前大学的时候用的string做出来的，哈哈，也不知是怎么写的
+        /// 时间复杂度：O(n)
+        /// 空间复杂度：O(n)
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                switch (s[i])
+                {
+                    case '(':
+                    case '[':
+                    case '{': stack.Push(s[i]); break;
+                    case ')':
+                    case ']':
+                    case '}':
+                        if (stack.Count == 0)
+                            return false;
+                        char c = stack.Pop();
+                        if ((c == '(' && s[i] == ')') || (c == '[' && s[i] == ']') || (c == '{' && s[i] == '}'))
+                            break;
+                        else
+                            return false;
+                    default: break;
+
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -17,24 +17,80 @@ namespace LeetCode
                 //int input = int.Parse(Console.ReadLine());
                 //int input2 = int.Parse(Console.ReadLine());
                 //int input3 = int.Parse(Console.ReadLine());
-                //string input = Console.ReadLine();
-                ////string input2 = Console.ReadLine();
+                string input = Console.ReadLine();
+                string input2 = Console.ReadLine();
                 //int[] intArr = input.Split(',').Select(s => int.Parse(s)).ToArray();
                 //int input2 = int.Parse(Console.ReadLine());
                 //int[] intArr = new int[] { 1, 3, 2 };
-                int[] intArr = new int[] { 4, 2, 1, 3, 2, 6, 3 };
-                int[] intArr2 = new int[] { 4, 2, 1, 3, 2, 6, 3 };
-                solution.FindMedianSortedArrays(intArr, intArr2);
-                //ConsoleX.WriteLine(res);
+                //int[] intArr = new int[] { 4, 2, 1, 3, 2, 6, 3 };
+                //int[] intArr2 = new int[] { 4, 2, 1, 3, 2, 6, 3 };
+                var res = solution.StrStr(input, input2);
+                ConsoleX.WriteLine(res);
             }
         }
 
         public class Solution
         {
-            public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+            public int StrStr(string haystack, string needle)
             {
+                if (needle == string.Empty)
+                    return 0;
 
+                int ans = -1;
+                for (int i = 0; i <= haystack.Length - needle.Length; i++)
+                {
+                    int j = 0;
+                    for (; j < needle.Length; j++)
+                    {
+                        if (haystack[i + j] != needle[j])
+                        {
+                            //sunday算法：不匹配，则查看 待匹配字符串 的后一位字符 c：1.若c存在于Pattern中，则 idx = idx + 偏移表[c] 2.否则，idx = idx + len(pattern)
+                            int nextStartIndex = needle.IndexOf(haystack[i + j]);
+                            if (nextStartIndex == -1)
+                                i = i + needle.Length;
+                            else
+                                i = i + nextStartIndex;
+                            break;
+                        }
+                    }
+                    if (j == needle.Length)
+                    {
+                        ans = i;
+                        break;
+                    }
+                }
+                return ans;
             }
+
+            /// <summary>
+            /// 时间复杂度：O(n)，可能这种比较好算的就要算最优和最差了吧，最优时间是O(n),最差时间是O((n-l)n)
+            /// 空间复杂度：O(1)
+            /// </summary>
+            /// <param name="haystack"></param>
+            /// <param name="needle"></param>
+            /// <returns></returns>
+            //public int StrStr(string haystack, string needle)
+            //{
+            //    if (needle == string.Empty)
+            //        return 0;
+
+            //    int ans = -1;
+            //    for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            //    {
+            //        int j = 0;
+            //        for (; j < needle.Length; j++)
+            //        {
+            //            if (haystack[i + j] != needle[j])
+            //                break;
+            //        }
+            //        if (j == needle.Length)
+            //        {
+            //            ans = i;
+            //            break;
+            //        }
+            //    }
+            //    return ans;
+            //}
         }
     }
 }
