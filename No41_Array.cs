a5a70d23f2dd4fa4740776ebc4835dbd3560eea9
atwood <atwood.so.interesting@gmail.com>
2020-06-19 17:42:32 +0800

    what a working day

--- No41_Array.cs
+++ No41_Array.cs
@@ -26,6 +26,14 @@ namespace LeetCode_41
 
     public class Solution
     {
+        /// <summary>
+        /// 在原数组基础上利用正负号储存信息，而且不影响本身的信息
+        /// 时间复杂度：O(n)
+        /// 空间复杂度：O(1),其实完全可以用一个O(n)的dictionary来做，这道题的关键就在于如果不开辟新空间，存贮老信息。
+        /// Experience：定长的数组问题可以使用正负号来储存额外信息
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
         public int FirstMissingPositive(int[] nums)
         {
             bool isIncludeOne = false;
