599295c79cdc2d1313c73f20c68ecc484592d2e0
atwood <atwood.so.interesting@gmail.com>
2020-06-22 17:40:44 +0800

    owe 19..

---
+++ No100_Tree.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_100
+{
+    //static void Main(string[] args)
+    //{
+    //    var solution = new Solution();
+    //    while (true)
+    //    {
+    //        //int input = int.Parse(Console.ReadLine());
+    //        //int input2 = int.Parse(Console.ReadLine());
+    //        //int input3 = int.Parse(Console.ReadLine());
+    //        //string input = Console.ReadLine();
+    //        //string input = "A man, a plan, a canal: Panama";
+    //        //string input2 = Console.ReadLine();
+    //        //int[] intArr = input.Split(',').Select(s => int.Parse(s)).ToArray();
+    //        //int input2 = int.Parse(Console.ReadLine());
+    //        //int[] intArr = new int[] { 1, 3, 2 };
+    //        //int[] intArr = new int[] { 1, 3 };
+    //        TreeNode tn1 = new TreeNode(1);
+    //        TreeNode tn2 = new TreeNode(2);
+    //        var res = solution.IsSameTree(tn1, tn2);
+    //        ConsoleX.WriteLine(res);
+    //    }
+    //}
+
+    public class Solution
+    {
+        /// <summary>
+        /// 看了其他题解，简化了一下代码，思想和下面的第一反应解是一样的
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+            else if (p == null || q == null || p.val != q.val)
+                return false;
+            else
+                return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+
+        /// <summary>
+        /// 递归解决
+        /// 时间复杂度：O(n)，其中 N 是树的结点数，因为每个结点都访问一次。
+        /// 空间复杂度：最优情况（完全平衡二叉树）时为 O(log(n))，最坏情况下（完全不平衡二叉树）时为 O(n)，用于维护递归栈。(复杂度也就是递归的深度)
+        /// 还有一个解法是通过遍历两棵树（好像是先序遍历），把节点分别放到两个记录的queue里，然后再来对比。
+        /// </summary>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        //public bool IsSameTree(TreeNode p, TreeNode q)
+        //{
+        //    if (p == null && q == null)
+        //        return true;
+        //    else if ((p == null && q != null) || (q == null && p != null))
+        //        return false;
+
+        //    if (IsTreeInSameState(p, q))
+        //    {
+        //        return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        //    }
+        //    else
+        //        return false;
+        //}
+
+        //private bool IsTreeInSameState(TreeNode p, TreeNode q)
+        //{
+        //    bool pLIsnull = p.left == null;
+        //    bool pRIsnull = p.right == null;
+        //    bool qLIsnull = q.left == null;
+        //    bool qRIsnull = q.right == null;
+        //    return p.val == q.val && pLIsnull == qLIsnull && pRIsnull == qRIsnull;
+        //}
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+}
---
+++ No10_Dp.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode_10
+{
+    //static void Main(string[] args)
+    //{
+    //    var solution = new Solution();
+    //    while (true)
+    //    {
+    //        //int input = int.Parse(Console.ReadLine());
+    //        //int input2 = int.Parse(Console.ReadLine());
+    //        //int input3 = int.Parse(Console.ReadLine());
+    //        //string input = Console.ReadLine();
+    //        //string input = "A man, a plan, a canal: Panama";
+    //        //string input2 = Console.ReadLine();
+    //        //int[] intArr = input.Split(',').Select(s => int.Parse(s)).ToArray();
+    //        //int input2 = int.Parse(Console.ReadLine());
+    //        //int[] intArr = new int[] { 1, 3, 2 };
+    //        //int[] intArr = new int[] { 1, 3 };
+    //        string input = "aab";
+    //        string input2 = "c*a*b";
+    //        var res = solution.IsMatch(input, input2);
+    //        ConsoleX.WriteLine(res);
+    //    }
+    //}
+
+    /// <summary>
+    /// REDO
+    /// </summary>
+    class Solution
+    {
+        /// <summary>
+        /// 动态规划，要仔细分析状态，然后写好转移方程才行。而且具体编码需要注意很多细节
+        /// Experience：高级方法对于低级方法来说就是降维打击，这道题有限确定状态机可以做，但是相较于动态规划而言就是刀耕火种，完全可以舍弃掉。
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public bool IsMatch(String s, String p)
+        {
+            int m = s.Length;
+            int n = p.Length;
+
+            bool[,] f = new bool[m + 1, n + 1];
+            f[0, 0] = true;
+            for (int i = 0; i <= m; ++i)
+            {
+                for (int j = 1; j <= n; ++j)
+                {
+                    if (p[j - 1] == '*')
+                    {
+                        f[i, j] = f[i, j - 2];
+                        if (matches(s, p, i, j - 1))
+                        {
+                            f[i, j] = f[i, j] || f[i - 1, j];
+                        }
+                    }
+                    else
+                    {
+                        if (matches(s, p, i, j))
+                        {
+                            f[i, j] = f[i - 1, j - 1];
+                        }
+                    }
+                }
+            }
+            return f[m, n];
+        }
+
+        public bool matches(String s, String p, int i, int j)
+        {
+            if (i == 0)
+            {
+                return false;
+            }
+            if (p[j - 1] == '.')
+            {
+                return true;
+            }
+            return s[i - 1] == p[j - 1];
+        }
+    }
+}
