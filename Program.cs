ec7b047ce23361142ce426c40fa5d8b01746ffe4
qldmw <atwood.l.lu@qq.com>
2020-08-05 01:12:20 +0800

    unsaved

--- Program.cs
+++ Program.cs
@@ -39,7 +39,26 @@ namespace LeetCode
         {
             public bool IsMatch(string s, string p)
             {
-
+                bool[,] dp = new bool[s.Length + 1, p.Length + 1];
+                dp[0, 0] = true;
+                for (int i = 1; i <= p.Length; i++)
+                {
+                    if (p[i - 1] == '*')
+                        dp[0, i] = true;
+                    else
+                        break;
+                }
+                for (int i = 1; i <= s.Length; i++)
+                {
+                    for (int j = 1; j <= p.Length; j++)
+                    {
+                        if (p[j - 1] == '*')
+                            dp[i, j] = dp[i - 1, j] || dp[i, j - 1];
+                        else if (p[j - 1] == '?' || s[i - 1] == p[j - 1])
+                            dp[i, j] = dp[i - 1, j - 1];
+                    }
+                }
+                return dp[s.Length, p.Length];
             }
         }
     }
