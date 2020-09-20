bd345c7dbcb235218617c636ecb76499786acd74
qldmw <atwood.l.lu@qq.com>
2020-09-21 00:21:28 +0800

    learn mutilThreading

--- Interview/Lock.cs
+++ Interview/Lock.cs
@@ -8,6 +8,11 @@ namespace LeetCode.Interview
 {
     class Lock
     {
+        /// <summary>
+        /// Knowledge:
+        /// 1.对于互斥锁，如果资源已经被占用，资源申请者只能进入睡眠状态。但是自旋锁不会引起调用者睡眠，如果自旋锁已经被别的执行单元保持，调用者
+        ///     就一直循环在那里看是否该自旋锁的保持者已经释放了锁，“自旋”一词就是因此而得名。https://www.cnblogs.com/kuliuheng/p/4064680.html
+        /// </summary>
         public class Solution
         {
             private static int _count = 0;
@@ -26,6 +31,9 @@ namespace LeetCode.Interview
                 //https://docs.microsoft.com/zh-cn/dotnet/api/system.threading.spinlock?view=netcore-3.1
                 //值类型的轻量级锁，在锁的粒度较大且数量较大时(例如，链接列表中的每个节点的锁) 或锁保持时间始终极短时，自旋锁可能非常有利。
                 //自旋锁会不断竞争，不像lock在竞争到一定次数后会休眠，所以不适用于长期占有锁的场景。
+                //自旋锁会保持线程，不会释放，所以当 运算时间 < 上下文切换时间 时，自旋锁就可以节省性能。
+
+                //自旋锁还有一个优化，SpinWait,它会交出线程控制，避免CPU被耗尽。
                 bool locked = false;
                 try
                 {
--- No1114_MultiThreading.cs
+++ No1114_MultiThreading.cs
@@ -8,42 +8,122 @@ namespace LeetCode_1114
     class No1114_MultiThreading
     {
         /// <summary>
-        /// 检查唯一变量，线程休眠
+        /// 使用 SpinWait 和 VolatileWrite，VolatileRead来解决
+        /// 其实这里可以不用 Volatile 的方法，直接使用 int 就可以。MSDN对 Volatile 的介绍：在多处理器系统上， VolatileWrite 确保写入内存位置的值
+        /// 立即对所有处理器都可见。 https://docs.microsoft.com/zh-cn/dotnet/api/system.threading.thread.volatilewrite?view=netcore-3.1
         /// </summary>
         public class Foo
         {
+            /// <summary>
+            /// Unsolved Question:什么时候应该使用 Volatile 方法
+            /// </summary>
+            private SpinWait _spinWait = new SpinWait();
+            private int _continueCondition = 1;
 
             public Foo()
             {
 
             }
-
-            //Unsolved Question:这里加了static就不能保证顺序输出了，不知道怎么回事，应该是一样的啊，不确定是不是OJ运行环境的问题。
-            //private static int _signal = 0;
-            private int _signal = 0;
             public void First(Action printFirst)
             {
-                // printFirst() outputs "first". Do not change or remove this line.
                 printFirst();
-                _signal = 1;
+                Thread.VolatileWrite(ref _continueCondition, 2);//写栅栏
             }
 
             public void Second(Action printSecond)
             {
-                while (_signal != 1)
-                    Thread.Sleep(1);
-                // printSecond() outputs "second". Do not change or remove this line.
+
+                while (Thread.VolatileRead(ref _continueCondition) != 2)
+                {
+                    _spinWait.SpinOnce();
+                }
                 printSecond();
-                _signal = 2;
+                Thread.VolatileWrite(ref _continueCondition, 3);//写栅栏
             }
 
             public void Third(Action printThird)
             {
-                while (_signal != 2)
-                    Thread.Sleep(1);
-                // printThird() outputs "third". Do not change or remove this line.
+                while (Thread.VolatileRead(ref _continueCondition) != 3)
+                {
+                    _spinWait.SpinOnce();
+                }
                 printThird();
+                Thread.VolatileWrite(ref _continueCondition, 1);//写栅栏
             }
         }
+
+        ///// <summary>
+        ///// AutoResetEvent实现线程通信
+        ///// </summary>
+        //public class Foo
+        //{
+        //    private AutoResetEvent _second = new AutoResetEvent(false);
+        //    private AutoResetEvent _three = new AutoResetEvent(false);
+
+        //    public Foo()
+        //    {
+
+        //    }
+
+        //    public void First(Action printFirst)
+        //    {
+        //        // printFirst() outputs "first". Do not change or remove this line.
+        //        printFirst();
+        //        _second.Set();
+        //    }
+
+        //    public void Second(Action printSecond)
+        //    {
+        //        _second.WaitOne();
+        //        // printSecond() outputs "second". Do not change or remove this line.
+        //        printSecond();
+        //        _three.Set();
+        //    }
+
+        //    public void Third(Action printThird)
+        //    {
+        //        _three.WaitOne();
+        //        // printThird() outputs "third". Do not change or remove this line.
+        //        printThird();
+        //    }
+        //}
+
+        ///// <summary>
+        ///// 检查唯一变量，线程休眠
+        ///// </summary>
+        //public class Foo
+        //{
+        //    public Foo()
+        //    {
+
+        //    }
+
+        //    //Unsolved Question:这里加了static就不能保证顺序输出了，不知道怎么回事，应该是一样的啊，不确定是不是OJ运行环境的问题。
+        //    //private static int _signal = 0;
+        //    private int _signal = 0;
+        //    public void First(Action printFirst)
+        //    {
+        //        // printFirst() outputs "first". Do not change or remove this line.
+        //        printFirst();
+        //        _signal = 1;
+        //    }
+
+        //    public void Second(Action printSecond)
+        //    {
+        //        while (_signal != 1)
+        //            Thread.Sleep(1);
+        //        // printSecond() outputs "second". Do not change or remove this line.
+        //        printSecond();
+        //        _signal = 2;
+        //    }
+
+        //    public void Third(Action printThird)
+        //    {
+        //        while (_signal != 2)
+        //            Thread.Sleep(1);
+        //        // printThird() outputs "third". Do not change or remove this line.
+        //        printThird();
+        //    }
+        //}
     }
 }
