bd518abaee51ed2848d0b8b2a1a40e5895229a4b
Luwei Lu <luwei.l.lu@pwc.com>
2020-12-25 15:42:28 +0800

    simple greedy

--- No455_Greedy.cs
+++ No455_Greedy.cs
@@ -1,57 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Extension;
-using LeetCode.ExtensionFunction;
-using System.Threading.Tasks;
-using System.Threading;
 
-namespace LeetCode
+namespace LeetCode_455
 {
-    class Program
+    class No455_Greedy
     {
-        static void Main(string[] args)
-        {
-            var solution = new Solution();
-            while (true)
-            {
-                //int input = int.Parse(Console.ReadLine());
-                //int input2 = int.Parse(Console.ReadLine());
-                //int input3 = int.Parse(Console.ReadLine());
-                //string input = Console.ReadLine();
-                //string input2 = Console.ReadLine();
-                //int[] intArr = input.Split(',').Select(s => int.Parse(s)).ToArray();
-                //int input2 = int.Parse(Console.ReadLine());
-                //var builder = new DataStructureBuilder();
-                //int?[] data = new int?[] { 1, 2, 3, 4, 5, null, 6, null, null, 7, 8 };
-                //var tree = builder.BuildTree(data);
-                //var listNode = builder.BuildListNode(new int[] { 1, 4, 5 });
-                //int[][] arr = new int[3][] { new int[] { 1, 3, 1 }, new int[] { 1, 5, 1 }, new int[] { 4, 2, 1 } };
-                //string input = "abcbefga";
-                //string input2 = "dbefga";
-                //int[] nums1 = new int[] { 1, 2, 3 };
-                //int[] nums2 = new int[] { 1, 1 };
-                //IList<IList<int>> data = new List<IList<int>>()
-                //{
-                //    new List<int>() { 1, 3 },
-                //    new List<int>() { 3, 0, 1 },
-                //    new List<int>() { 2 },
-                //    new List<int>() { 0 }
+        //static void Main(string[] args)
+        //{
+        //    var solution = new Solution();
+        //    while (true)
+        //    {
+        //        //int input = int.Parse(Console.ReadLine());
+        //        //int input2 = int.Parse(Console.ReadLine());
+        //        //int input3 = int.Parse(Console.ReadLine());
+        //        //string input = Console.ReadLine();
+        //        //string input2 = Console.ReadLine();
+        //        //int[] intArr = input.Split(',').Select(s => int.Parse(s)).ToArray();
+        //        //int input2 = int.Parse(Console.ReadLine());
+        //        //var builder = new DataStructureBuilder();
+        //        //int?[] data = new int?[] { 1, 2, 3, 4, 5, null, 6, null, null, 7, 8 };
+        //        //var tree = builder.BuildTree(data);
+        //        //var listNode = builder.BuildListNode(new int[] { 1, 4, 5 });
+        //        //int[][] arr = new int[3][] { new int[] { 1, 3, 1 }, new int[] { 1, 5, 1 }, new int[] { 4, 2, 1 } };
+        //        //string input = "abcbefga";
+        //        //string input2 = "dbefga";
+        //        //int[] nums1 = new int[] { 1, 2, 3 };
+        //        //int[] nums2 = new int[] { 1, 1 };
+        //        //IList<IList<int>> data = new List<IList<int>>()
+        //        //{
+        //        //    new List<int>() { 1, 3 },
+        //        //    new List<int>() { 3, 0, 1 },
+        //        //    new List<int>() { 2 },
+        //        //    new List<int>() { 0 }
 
-                //    //new List<int>() { 1 },
-                //    //new List<int>() { 2 },
-                //    //new List<int>() { 3 },
-                //    //new List<int>() {  }
-                //};
+        //        //    //new List<int>() { 1 },
+        //        //    //new List<int>() { 2 },
+        //        //    //new List<int>() { 3 },
+        //        //    //new List<int>() {  }
+        //        //};
 
-                int[] nums1 = new int[] { 10, 9, 8, 7, 10, 9, 8, 7 };
-                int[] nums2 = new int[] { 10, 9, 8, 7 };
-                var res = solution.FindContentChildren(nums1, nums2);
-                ConsoleX.WriteLine(res);
-            }
-        }
+        //        int[] nums1 = new int[] { 10, 9, 8, 7, 10, 9, 8, 7 };
+        //        int[] nums2 = new int[] { 10, 9, 8, 7 };
+        //        var res = solution.FindContentChildren(nums1, nums2);
+        //        ConsoleX.WriteLine(res);
+        //    }
+        //}
 
         public class Solution
         {
